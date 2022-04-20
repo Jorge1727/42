6655f17a4f91603da683d930b619dfa0a9cea7d7
jorge1727 <jorge1727.jt@gmail.com>
2022-04-20 19:43:17 +0200

    agrego el 42

---
+++ cursus/libft.a/libft.h
@@ -0,0 +1,8 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+# define BUFFER_SIZE 1
---
+++ practicando/arrays/arrays_pi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int ac, char **av)
+{
+    int[] edades = new int[3];
+
+    edades[0] = 15;
+    edades[1] = 30;
+    edades[2] = 35;
+
+    Console.WriteLine(edades[1]);
+}
